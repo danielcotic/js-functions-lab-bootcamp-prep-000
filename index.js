2d807acc4de5fc217d69fa65e9dcfb269eb3f11d
Daniel Cotic <d.cotic26@gmail.com>
2017-12-07 04:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(name, holiday){
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
